09ddd6e2495fe14407d94999a56e31003c9d88ce
Darke Maciel <darke_maciel@hotmail.com>
2024-05-28 09:11:52 -0300

    Ajuste no script de processamento de tabelas, novas figuras, adicionando novas colunas na tabela fato receita.

--- DDL e DML/Script - processamento das tabelas.sql
+++ DDL e DML/Script - processamento das tabelas.sql
@@ -4,5 +4,23 @@ ALTER TABLE [STG].[dbo].[stg_despesas_processada] DROP COLUMN rank;
 
 ALTER TABLE [STG].[dbo].[stg_receitas_processada] DROP COLUMN rank;
 
+
 -- Alteração na estrutura das tabelas de receita e despesa, convertendo de string para numérico.
 
+ALTER TABLE [STG].[dbo].[stg_receitas_processada]
+ADD num_mes_ano NUMERIC(6, 0)
+
+UPDATE [STG].[dbo].[stg_receitas_processada]
+SET num_mes_ano = CAST(FORMAT(CAST(data_arquivo AS DATE), 'yyyyMM') AS NUMERIC)
+
+UPDATE [STG].[dbo].[stg_receitas_processada]
+SET arrecadada = CAST(REPLACE(arrecadada, ',', '.') AS decimal(15, 2))
+
+ALTER TABLE [STG].[dbo].[stg_receitas_processada]
+ALTER COLUMN arrecadada DECIMAL(15,2)
+
+UPDATE [STG].[dbo].[stg_receitas_processada]
+SET arrecadada_fundeb = CAST(REPLACE(arrecadada_fundeb, ',', '.') AS decimal(15, 2))
+
+ALTER TABLE [STG].[dbo].[stg_receitas_processada]
+ALTER COLUMN arrecadada_fundeb DECIMAL(15,2)
